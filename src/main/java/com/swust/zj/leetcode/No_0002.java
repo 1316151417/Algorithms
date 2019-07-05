afa93c99d9cc74d0f6227a5f8fc9bf7481c7befe
zhoujie1 <zhoujie1@100tal.com>
2019-07-05 14:00:30 +0800

    leetcode 0002

---
+++ src/main/java/com/swust/zj/leetcode/No_0002.java
@@ -0,0 +1,33 @@
+package com.swust.zj.leetcode;
+
+public class No_0002 {
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode root = null;
+        ListNode cursor = null;
+        int carry = 0;
+        while (l1 != null || l2 != null || carry == 1) {
+            int val = (l1 == null ? 0 : l1.val) + (l2 == null ? 0 : l2.val) + carry;
+            carry = val / 10;
+            ListNode newNode = new ListNode(val % 10);
+            if (root == null) {
+                root = newNode;
+                cursor = root;
+            } else {
+                cursor.next = newNode;
+                cursor = cursor.next;
+            }
+            l1 = l1 == null ? null : l1.next;
+            l2 = l2 == null ? null : l2.next;
+        }
+        return root;
+    }
+}
